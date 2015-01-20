4b398bf3a2ca25e9ed28ac9388f0736c980a2bf4
Pedro Alvarado Rincon <pwalvarado@gmail.com>
2015-01-20 09:23:54 -0500

    Adding authentication

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,4 +1,6 @@
 class RecipesController < ApplicationController
+  before_filter :authenticate_user!, only: [:new, :edit, :create, :update, :destroy]
+
   before_action :set_recipe, only: [:show, :edit, :update, :destroy]
 
   # GET /recipes
