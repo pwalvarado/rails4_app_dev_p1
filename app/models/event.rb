5792a5ca3d8a7970d841f3e21e98298f3925c905
Pedro Alvarado Rincon <pwalvarado@gmail.com>
2015-01-23 17:10:10 -0500

    Adding tags to events skills: - virtual attributes in a form (getters and setter) - association models many to many - defining indexes on migrations

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,6 +3,9 @@ class Event < ActiveRecord::Base
   belongs_to :organizer, :foreign_key => :organizer_id, class_name: "User"
   #incorrecto:
   #has_one :organizer, :foreign_key => :organizer_id, class_name: "User"
+  has_many :taggings
+  has_many :tags, through: :taggings
+
 
   extend FriendlyId
   friendly_id :slug_candidates, use: :slugged
@@ -15,6 +18,16 @@ class Event < ActiveRecord::Base
       [:title, :start_date],
       [:title, :start_date, :location]
     ]
+  end
+
+  def all_tags=(names)
+    self.tags = names.split(",").map do |t|
+      Tag.where(name: t.strip).first_or_create!
+    end
+  end
+
+  def all_tags
+    self.tags.map(&:name).join(' ')
   end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150123165502) do
+ActiveRecord::Schema.define(version: 20150123205804) do
 
   create_table "cuisines", force: true do |t|
     t.string   "name"
@@ -66,6 +66,22 @@ ActiveRecord::Schema.define(version: 20150123165502) do
     t.integer  "cuisine_id"
     t.text     "ingredients"
     t.text     "procedure"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "taggings", force: true do |t|
+    t.integer  "tag_id"
+    t.integer  "event_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "taggings", ["event_id"], name: "index_taggings_on_event_id", using: :btree
+  add_index "taggings", ["tag_id"], name: "index_taggings_on_tag_id", using: :btree
+
+  create_table "tags", force: true do |t|
+    t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
